38692f49346f87ff758e83adfd37c7a2e143ca0d
praveenr2025 <Praveen.R@nilehospitality.com>
2025-11-06 13:27:54 +0530

    Update appointments.tsx

--- frontend/pages/clinic/appointments.tsx
+++ frontend/pages/clinic/appointments.tsx
@@ -90,7 +90,7 @@ useEffect(() => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/clinic/appointments", {
+      const res = await fetch("https://bankreconn.centralindia.cloudapp.azure.com/api/clinic/appointments", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
